ca926ffa2574a745b9d8f256b2850a946291f3a6
Reception2 <Reception2@DUNLRECEPTILL>
2022-10-06 10:03:59 +0100

    Add project files.

---
+++ switch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********* Switch Statement ***********\n");
+
+            int num;
+
+
+            Console.Write("Enter a number (1-3): ");
+            num = int.Parse(Console.ReadLine());
+
+            switch(num)
+            {
+
+                case 1:
+                    Console.WriteLine("You have selected the first statement");
+                    break;
+                case 2:
+                    Console.WriteLine("You have selected the second statement");
+                    break;
+                case 3:
+                    Console.WriteLine("You have selected the third statement");
+                    break;
+                default:
+                    Console.WriteLine("You have entered an invalid number");
+                    break;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
